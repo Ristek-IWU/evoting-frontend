2a8099e97b6fe04da45ec79c1a220829016a92da
Cubee-kun <ibraveaslam73@gmail.com>
2026-02-09 23:31:46 +0700

    update

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://api.evotting.himaif-iwu.com",
 });
 
 // 🔐 Ambil token USER atau ADMIN otomatis
